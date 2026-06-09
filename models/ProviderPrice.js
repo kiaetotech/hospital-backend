300ba05ad7ba8eb361845a5267a846ac8ab89fd5
Kiaeto tech <kiaetotech@gmail.com>
2026-06-09 19:36:19 +0530

    Added providerId field to ProviderPrice model

--- models/ProviderPrice.js
+++ models/ProviderPrice.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const providerPriceSchema = new mongoose.Schema({
+  providerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Provider', required: true },
   providerName: { type: String, required: true },
   testName: { type: String, required: true },
   price: { type: Number, required: true },
@@ -13,6 +14,7 @@ const providerPriceSchema = new mongoose.Schema({
   updatedAt: { type: Date, default: Date.now }
 });
 
-providerPriceSchema.index({ testName: 1, providerName: 1 });
+// Create compound index for faster queries
+providerPriceSchema.index({ providerId: 1, testName: 1 });
 
 module.exports = mongoose.model('ProviderPrice', providerPriceSchema);
