00097e0c76b804f43dd9baff6011a454ac6c8747
Kiaeto tech <kiaetotech@gmail.com>
2026-07-23 12:02:09 +0530

    Add TestPricing and TestMaster refs back inside route

--- routes/hospitalProvider.js
+++ routes/hospitalProvider.js
@@ -1875,7 +1875,8 @@ router.put('/id/:providerId/beds', authenticateHospital, async (req, res) => {
 router.get('/corporate/services', authenticateHospital, async (req, res) => {
   try {
     const hospitalId = req.user._id;
-        
+    const TestPricing = require('mongoose').model('TestPricing');
+    const TestMaster = require('mongoose').model('TestMaster');    
     const labTests = await TestPricing.find({ provider_id: hospitalId })
       .populate('test_id', 'test_name test_code major_category')
       .lean();
